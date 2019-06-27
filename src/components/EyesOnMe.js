6847ddd1b0a26755ec626593f461c669f809e709
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-27 09:42:46 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+
+class EyesOnMe extends Component{
+    constructor(){
+        super()
+        this.state = {
+            password: ''
+        }
+    }
+
+    good = () =>{
+        return console.log('Good!')
+    }
+    bad = () =>{
+        return console.log('Hey! Eyes on me!')
+    }
+    render(){
+        return (
+        <div>
+            <button onFocus={this.good} onBlur={this.bad}>Click Me!</button>
+        </div>
+        )}
+}
+
+export default EyesOnMe;
