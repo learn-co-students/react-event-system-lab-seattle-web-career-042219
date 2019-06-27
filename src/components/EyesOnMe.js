e793f33362fbda70be0a782fe437a6e941b70c52
Bureaucratix <bureaucratix@gmail.com>
2019-06-27 16:43:04 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+class EyesOnMe extends Component {
+    render() {
+        return(
+           <button onFocus={this.focusHandler} onBlur={this.blurHandler} />
+        )
+    }
+
+    focusHandler =() => {
+        console.log("Good!")
+    }
+
+    blurHandler= () => {
+        console.log("Hey! Eyes on me!")
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class CLASSNAME extends Component {
+
+    handleEnter = () => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return (
+            <input type="password" onKeyUp={this.handleEnter}/>
+        )
+    }
+}
+
+export default CLASSNAME;
